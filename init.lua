9f22fa974e2ab4edf87ec6d730f03ed1dac22a45
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-15 21:24:31 +0200

    V2 of my nvim config with lazy vim

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- enable system clipboard
+vim.opt.clipboard:append("unnamedplus")
---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,12 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  opts = {
+    user_default_options = {
+      names = true,
+      rgb_fn = true,
+      hsl_fn = true,
+      css = true,
+      mode = "virtualtext",
+    },
+  },
+}
