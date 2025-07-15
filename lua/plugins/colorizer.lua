d4009d37e4d57bdb3a229c31973dc7e48e20f142
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-15 21:31:23 +0200

    Import BreadOnPenguins version see her github:
    https://github.com/BreadOnPenguins/nvim
    for full details

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,12 +1,10 @@
-return {
-  "NvChad/nvim-colorizer.lua",
-  opts = {
-    user_default_options = {
-      names = true,
-      rgb_fn = true,
-      hsl_fn = true,
-      css = true,
-      mode = "virtualtext",
-    },
-  },
-}
+require("colorizer").setup({ "*" }, {
+	RGB = true, -- #RGB hex codes
+	RRGGBB = true, -- #RRGGBB hex codes
+	names = false, -- "Name" codes like Blue
+	RRGGBBAA = true, -- #RRGGBBAA hex codes
+	rgb_fn = true, -- CSS rgb() and rgba() functions
+	hsl_fn = true, -- CSS hsl() and hsla() functions
+	css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+})
